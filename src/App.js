20fb8e78529a81ca248813d58081c2d0a7920e41
CHANUKA RANDITHA <155728444+Chanuka999@users.noreply.github.com>
2024-12-31 20:38:28 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
+import { useState } from "react";
+
 const App = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [count, setCount] = useState(0);
   const getQuotes = async () => {
     try {
       const res = await fetch("https://api.quotable.io/quotes/random");
       const data = await res.json();
-      console.log(data);
-      console.log(data[0].author);
-      console.log(data[0].content);
+      // console.log(data);
+      // console.log(data[0].author);
+      // console.log(data[0].content);
+      const randomQuote = data[0];
+      console.log(randomQuote);
+      setQuote(randomQuote.content);
+      setAuthor(randomQuote.author);
+      setCount((c) => c + 1);
     } catch {
       console.error("Error fetching quotes", error);
     }
   };
   return (
     <>
-      <h1>Lets get motivated by reading quotes</h1>
+      <h1>{quote}</h1>
+      <p>
+        <strong>Autor:</strong>
+        {author}
+      </p>
       <button onClick={getQuotes}>New Quotes</button>
+      <p>
+        You have read <strong>{count}</strong>quotes
+      </p>
     </>
   );
 };
